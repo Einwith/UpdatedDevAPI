801f3e36ab1b018000fd54c005d15bd7a2749d77
Einwith <prank1363246@yahoo.com>
2021-05-09 21:43:18 -0300

    Initial setup

---
+++ src/main/java/com/info/commons/UpdatedDevAPI/UpdatedDevApiApplication.java
@@ -0,0 +1,13 @@
+package com.info.commons.UpdatedDevAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatedDevApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatedDevApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/info/commons/UpdatedDevAPI/UpdatedDevApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.commons.UpdatedDevAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdatedDevApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
